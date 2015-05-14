40f426fc65312aa21c56f00b1cc7c5db3c494488
leathiebault <lea.thiebault@yahoo.fr>
2015-05-14 10:14:40 +0200

    second commit

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fooApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the fooApp
+ */
+angular.module('fooApp')
+  .controller('ContactCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
